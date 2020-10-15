f2ecce636e0f47060f38f4a87333a746c24a749f
miguelcoria94 <miguelcoria94@gmail.com>
2020-10-15 16:49:14 -0500

    donw with project

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ document.addEventListener("DOMContentLoaded", event => {
     
     let firstColor = document.querySelector(".main__container-color1")
     let secondColor = document.querySelector(".main__container-color2")
+    let colorReader = document.querySelector(".main__container-color-reader")
 
-    function updateColor
+    function updateColor() {
+        document.body.style.background = `linear-gradient(to right, ${firstColor.value} , ${secondColor.value})`
+        colorReader.innerHTML = `gradient(to right,${firstColor.value }, ${secondColor.value})`
+    }
 
     firstColor.addEventListener('change', target => {
-        document.body.style.background = `linear-gradient(to right, ${firstColor.value} , ${secondColor.value})`
+        updateColor()
     })
 
     secondColor.addEventListener('change', target => {
-        document.body.style.background = `linear-gradient(to right, ${firstColor.value} , ${secondColor.value})`
+        updateColor()
     })
 })
